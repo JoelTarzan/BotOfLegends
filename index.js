2f8b95893485724291747ab03ff6bc90cd632c18
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2022-09-09 09:11:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const client = new Discord.Client({
 });
 const CONFIG = require('./config.json');
 
-// Prefijo del bot
 const prefix = '<';
 
-// Mensaje de ayuda
 const help = `Manual de ayuda Bot Of Legends
 
 -----------------Comandos--------------
@@ -19,45 +17,33 @@ const help = `Manual de ayuda Bot Of Legends
 version: 1.0
 author: PRECINTO`;
 
-// Mensaje en el canal al encender el bot.
 client.on('ready', () => {
     client.channels.cache.get('971894850670706719').send('Bot Of Legends ha sido invocado')
 });
 
-// Aviso por consola cuando se enciende el bot
 client.login(CONFIG.token).then(() => console.log(`${client.user.username} se ha conectado.`));
 
-// Comandos
 client.on('messageCreate', message => {
 
-    // Me aseguro de que el mensaje empieza por el prefijo
     if (!message.content.startsWith(prefix)) return;
 
-    // Copio el contenido del mensaje
     var mensaje = message.content;
 
-    // Cambio el contenido a String
     mensaje.toString;
 
-    // Le quito el prefijo
     var frase = mensaje.slice(1);
 
-    // Separo el comando de los argumentos
     var comandos = frase.split(" ");
 
-    // Si el comando es help
     if (comandos[0] == "help") {
         message.reply(help);
 
-        // Si el comando es aram
     } else if (comandos[0] == "aram") {
         message.reply('https://www.op.gg/modes/aram/' + comandos[1] + '/build');
-
-        // Si el comando es sum
+        
     } else if (comandos[0] == "sum") {
         message.reply('https://euw.op.gg/summoners/euw/' + comandos[1]);
 
-        // Si no es ninguno de esos comandos, y la array solo contiene una palabra, será el champion
     } else if (comandos.length == 1) {
         message.reply('https://www.op.gg/champions/' + comandos[0])
 
